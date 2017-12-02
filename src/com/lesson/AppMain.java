72d04f40ff8e9ee7ea3d9e460817f2f2fd58e471
higuchi <pirurotta@gmail.com>
2017-12-02 18:13:47 +0900

    コミット

--- src/com/lesson/AppMain.java
+++ src/com/lesson/AppMain.java
@@ -9,21 +9,37 @@ import java.io.InputStreamReader;
 public class AppMain {
 
 	public static void main(String[] args) {
-		 try {
-		      File f = new File("C:\\Users\\higuchi0929\\Desktop\\data.csv");
-		      BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(f),"SJIS"));
+		try {
+			File f = new File("C:\\Users\\higuchi0929\\Desktop\\data.csv");
+			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(f), "SJIS"));
 
-		      String line;
-		      while ((line = br.readLine()) != null) {
-		        String[] data = line.split(",", 0);
+			String line;
+			while ((line = br.readLine()) != null) {
+				String[] data = line.split(",", 0);
+				String code = data[3];
+				System.out.println(code);
 
-		        for (String elem : data) {
-		          System.out.println(elem);
-		        }
-		      }
-		      br.close();
+				if(code.equals("1")){
+					System.out.println("社員コード："+data[0]+"，社員名："+data[1]+"，部門名："+data[2]+"，採用担当です");
+				};
 
-		    } catch (IOException e) {
-		      System.out.println(e);
-		    }
-}}
+				if(code.equals("2")){
+					System.out.println("社員コード："+data[0]+"，社員名："+data[1]+"，部門名："+data[2]+"，総務担当です");
+				};
+
+				if(code.equals("3")){
+					System.out.println("社員コード："+data[0]+"，社員名："+data[1]+"，部門名："+data[2]+"，開発担当です");
+				};
+
+
+				/*
+				 * for (String elem : data) { System.out.println(elem); }
+				 */
+			}
+			br.close();
+
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+	}
+}
